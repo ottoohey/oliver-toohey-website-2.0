5a22974be94a9a8ac5a680f0325fb384198162d2
ottoohey <ottoohey@gmail.com>
2023-02-26 14:06:53 +1100

    Nav bar improved

--- src/components/Home/TitlescreenBio.js
+++ src/components/Home/TitlescreenBio.js
@@ -6,35 +6,35 @@ export default function TitlescreenBio({ isMobile }) {
   );
   return (
     <>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[0] ? "text-opaque" : "text-clear"}>
           Hello there, welcome to my website.
         </p>
       </div>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[1] ? "text-opaque" : "text-clear"}>
           My name is Oliver Toohey and I'm a Software Developer based in
           Melbourne, Australia. React vs Flutter - porque no los dos?
         </p>
       </div>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[2] ? "text-opaque" : "text-clear"}>
           My head gets stuck in the clouds sometimes (haha), mainly AWS and
           Azure.
         </p>
       </div>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[3] ? "text-opaque" : "text-clear"}>
           Need something automated? I can do that with Ansible, at least until
           ChatGPT takes over.
         </p>
       </div>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[4] ? "text-opaque" : "text-clear"}>
           I love to travel and want to work overseas one day.
         </p>
       </div>
-      <div className="fixed mx-auto inset-x-0 top-24 text-sm sm:text-lg">
+      <div className="fixed mx-auto inset-x-0 top-32 text-sm sm:text-lg">
         <p className={opacityArray[5] ? "text-opaque" : "text-clear"}>
           Nice to meet you.
         </p>
--- src/components/Navigation/NavigationBanner.js
+++ src/components/Navigation/NavigationBanner.js
@@ -1,38 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 export default function NavigationBanner({ scrollDownToContactMe }) {
-  const [width, setWidth] = useState(window.innerWidth);
-
-  function handleWindowSizeChange() {
-    setWidth(window.innerWidth);
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowSizeChange);
-    return () => {
-      window.removeEventListener("resize", handleWindowSizeChange);
-    };
-  }, []);
-
-  const isMobile = width <= 768;
-
   return (
     <>
-      <div className="fixed mx-auto inset-x-0 -top-1 bg-zinc-800 h-24 z-40">
-        <p className="fixed uppercase mx-auto inset-x-0 top-12 text-2xl text-yellow-100">
-          olivertoohey.com
-        </p>
-        <button
-          className={
-            isMobile
-              ? "opacity-0"
-              : "fixed uppercase top-12 right-12 text-xs text-yellow-100 hover:font-bold"
-          }
-          onClick={scrollDownToContactMe}
-        >
-          contact me
-        </button>
+      <div className="fixed mx-auto inset-x-0 -top-1 bg-zinc-800 h-32 z-40">
+        <div className="grid grid-cols-1 h-10 m-auto">
+          <p className="uppercase mx-auto inset-x-0 mt-12 text-2xl text-yellow-100">
+            olivertoohey.com
+          </p>
+          <div className="flex mx-auto gap-4 p-4">
+            <button className="uppercase text-xs text-yellow-100 hover:font-bold">
+              <Link to="/">home</Link>
+            </button>
+            <button className="uppercase text-xs text-yellow-100 hover:font-bold">
+              <Link to="/blogs/1">blogs</Link>
+            </button>
+            <button
+              className="uppercase text-xs text-yellow-100 hover:font-bold"
+              onClick={scrollDownToContactMe}
+            >
+              contact me
+            </button>
+          </div>
+        </div>
       </div>
       <Outlet />
     </>
